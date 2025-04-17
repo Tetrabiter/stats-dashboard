6ce8ba8e91b24f29f5c52bebbd2c7949f5e9b1ac
Tetrabiter <noa.eskenkot@gmail.com>
2025-04-17 05:48:41 -0400

    start landing page

---
+++ src/pages/ LandingPage.tsx
@@ -0,0 +1,38 @@
+import Logo from "../shared/ui/Logo";
+
+
+const LandingPage = () => {
+    return ( 
+        <div className="">
+            <header>
+                <Logo />
+                <button>Get Started</button>
+            </header>
+
+
+            <main>
+                <div>
+                    <p></p>
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    <p></p>
+                    <div>
+                        
+                    </div>
+                </div>
+            </main>
+
+
+            <footer>
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+ 
+export default LandingPage;
---
+++ src/shared/ui/Logo.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return ( 
+        <div>
+            <img src="" alt="Re.Bots logotype" />
+        </div>
+    );
+}
+ 
+export default Logo;
